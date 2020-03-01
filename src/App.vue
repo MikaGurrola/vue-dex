7f4438c0697af51a85e774c27495929c67fd41a9
Mika <mikagurrola90@gmail.com>
2020-03-01 13:23:36 -0600

    initialized project

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="The quick brown fox jumped over the lazy dog."/>
   </div>
 </template>
 
